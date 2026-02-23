743a069127a810fcb739aaf603879321fd85d3cd
SyedTawsif07 <sayedtawsif03@gmail.com>
2026-02-24 01:36:55 +0600

    nothing changes

--- concep.js
+++ concep.js
@@ -14,6 +14,7 @@ const allFilterBtn = document.getElementById('all-filter-btn');
 const interviewFilterBtn = document.getElementById('interview-filter-btn');
 const rejectedFilterBtn = document.getElementById('rejected-filter-btn');
 
+
 function calculateCount() {
     total.innerText = allCardCount.children.length
     interviewCount.innerText = interviewList.length
